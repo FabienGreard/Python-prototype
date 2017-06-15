503b140b52ecccf88dc0bbd7bf4f0a72e236cf87
Fabien <grd.fabien@gmail.com>
2017-06-15 13:23:11 +0200

    Innit

--- Python prototype/Python_prototype.py
+++ Python prototype/Python_prototype.py
@@ -1,1 +1,17 @@
 
+import os
+import pygame
+from pygame.locals import *
+
+#initialize pygame
+pygame.init()
+
+#windows frame
+windows = pygame.display.set_mode((640,480), RESIZABLE)
+
+#keep the frame open
+while True:
+    key = input()
+    if key == 'q':
+        os.sys.exit(0)
+        break
